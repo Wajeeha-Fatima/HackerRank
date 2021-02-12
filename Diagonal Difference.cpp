cb3ef30e7f031160a1838a1ba4c94097baa47734
Wajeeha-Fatima <77614915+Wajeeha-Fatima@users.noreply.github.com>
2021-02-12 14:52:39 +0500

    Update Diagonal Difference.cpp

--- Diagonal Difference.cpp
+++ Diagonal Difference.cpp
@@ -14,7 +14,7 @@ int diagonalDifference(vector<vector<int>> arr) {
     
     for(int i=0; i<arr.size(); i++)
     {
-        for(int j=0; j<arr[i].size(); j++)
+        for(int j=0; j<arr.size(); j++)
         {
             if( i == j )
                 primary += arr[i][j];
