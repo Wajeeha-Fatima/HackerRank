606a60ee8c3c3da601950ea7ae2df87b4ff27e7d
Wajeeha-Fatima <77614915+Wajeeha-Fatima@users.noreply.github.com>
2021-02-14 22:21:34 +0500

    Create Between Two Sets.cpp

---
+++ Between Two Sets.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+int getTotalX(vector<int> a, vector<int> b) {
+    int count = 0;
+    
+    bool flag = true;
+    
+    for(int i=a.front(); i<=b.back(); i++)
+    {
+        flag = true;
+        
+        for(int j=0; j<a.size(); j++)
+        {
+            if( i%a[j] != 0 )
+            {
+                flag = false;
+                break;
+            }
+        }
+        for(int k=0; k<b.size(); k++)
+        {
+            if( b[k]%i != 0 )
+            {
+                flag = false;
+                break;
+            }
+        }
+        if(flag) 
+            count++;
+    }
+    return count;
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    vector<int> brr(m);
+
+    for (int i = 0; i < m; i++) {
+        cin >> brr[i];
+    }
+
+    int total = getTotalX(arr, brr);
+
+    cout << total << "\n";
+
+    return 0;
+}
