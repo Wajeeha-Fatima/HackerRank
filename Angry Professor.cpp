4de6b93b932334227db70716ca70dfaa438dbbc9
Wajeeha-Fatima <77614915+Wajeeha-Fatima@users.noreply.github.com>
2021-02-16 23:16:46 +0500

    Create Angry Professor.cpp

---
+++ Angry Professor.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the angryProfessor function below.
+string angryProfessor(int k, vector<int> a) {
+
+    int threshold = 0;
+    for(int i=0; i<a.size(); i++)
+    {
+        if(a[i] <= 0)
+            threshold++;
+    }
+    
+    if(threshold < k)
+        return "YES";
+    else
+        return "NO";
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        string result = angryProfessor(k, a);
+
+        cout << result << "\n";
+    }
+
+    return 0;
+}
