bec8187aa83326ac2aaf2d33c1d2dd8391291fde
Wajeeha-Fatima <77614915+Wajeeha-Fatima@users.noreply.github.com>
2021-02-17 00:27:25 +0500

    Create Drawing Book.cpp

---
+++ Drawing Book.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the pageCount function below.
+ */
+int pageCount(int n, int p) {
+    /*
+     * Write your code here.
+     */
+
+    int moves = n / 2;
+    int pages = p / 2;
+    
+    if(pages < moves/2+1)
+        return pages;
+    else
+        return moves - pages;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int p;
+    cin >> p;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = pageCount(n, p);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
